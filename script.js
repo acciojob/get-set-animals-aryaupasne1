8d2010468cc754c99669abc987f5cfd218962ae0
Arya Upasne <aryaupasne22@gmail.com>
2026-09-21 01:26:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// //complete this code
-// class Animal {}
-
-// class Dog extends Animal {}
-
-// class Cat extends Animal {}
-
-// // Do not change the code below this line
-// window.Animal = Animal;
-// window.Dog = Dog;
-// window.Cat = Cat;
 class Animal {
     constructor(species) {
         this._species = species;
